b2f5d424cfc78a1ad8cf706f2ef02be6b7265172
Ian Page Hands <iphands@gmail.com>
2018-01-08 14:40:48 -0500

    sass for matt

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/*global require*/
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
