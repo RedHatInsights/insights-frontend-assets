d214380ae8bd1e19ac8e7713f0e9fc7b3aae39fb
Ian Page Hands <iphands@gmail.com>
2018-01-08 13:59:12 -0500

    support JS for the splash page

--- js/splash.js
+++ js/splash.js
@@ -1,1 +1,21 @@
+/*global window, document*/
+'use strict';
+(function () {
+    var baseUrl = '/insightsbeta';
 
+    if (window.location.pathname === '/insights' || window.location.pathname === '/insights/') {
+        baseUrl = '/insights';
+    }
+
+    // rewrite the gotoapp anchor
+    document.querySelectorAll('a[href="/insights/overview/"]').forEach(function (a) {
+        a.href = baseUrl + '/overview/';
+    });
+
+    // if the user is authed bounce
+    window.require(['session'], function (session) {
+        if (session.isAuthenticated()) {
+            window.location = baseUrl + '/overview/';
+        }
+    });
+}());
